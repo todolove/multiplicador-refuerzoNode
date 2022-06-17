2390471c5d17bf18796014dfce7df529d9b67fc1
todolove <123256181+todolove@users.noreply.github.com>
2022-06-17 06:53:49 +0000

    cpufreq: interactive: Move definition of cpufreq_gov_interactive downwards

--- src/components/UserInfo/src/UserInfo.vue
+++ src/components/UserInfo/src/UserInfo.vue
@@ -23,4 +23,45 @@ const { replace } = useRouter()
 const loginOut = () => {
   ElMessageBox.confirm(t('common.loginOutMessage'), t('common.reminder'), {
     confirmButtonText: t('common.ok'),
-    cancelButtonText: t('common.cancel')
+    cancelButtonText: t('common.cancel'),
+    type: 'warning'
+  })
+    .then(async () => {
+      const res = await loginOutApi().catch(() => {})
+      if (res) {
+        wsCache.clear()
+        tagsViewStore.delAllViews()
+        resetRouter() // 重置静态路由表
+        replace('/login')
+      }
+    })
+    .catch(() => {})
+}
+
+const toDocument = () => {
+  window.open('https://element-plus-admin-doc.cn/')
+}
+</script>
+
+<template>
+  <ElDropdown :class="prefixCls" trigger="click">
+    <div class="flex items-center">
+      <img
+        src="@/assets/imgs/avatar.jpg"
+        alt=""
+        class="w-[calc(var(--logo-height)-25px)] rounded-[50%]"
+      />
+      <span class="<lg:hidden text-14px pl-[5px] text-[var(--top-header-text-color)]">Archer</span>
+    </div>
+    <template #dropdown>
+      <ElDropdownMenu>
+        <ElDropdownItem>
+          <div @click="toDocument">{{ t('common.document') }}</div>
+        </ElDropdownItem>
+        <ElDropdownItem divided>
+          <div @click="loginOut">{{ t('common.loginOut') }}</div>
+        </ElDropdownItem>
+      </ElDropdownMenu>
+    </template>
+  </ElDropdown>
+</template>
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import type { App } from 'vue'
+import { Icon } from './Icon'
+
+export const setupGl
