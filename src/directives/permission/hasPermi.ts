34623ecbe64406249d0c3a55b187215853c0ef49
todolove <123256181+todolove@users.noreply.github.com>
2022-06-24 07:09:19 +0000

    more exception handling

--- src/directives/permission/hasPermi.ts
+++ src/directives/permission/hasPermi.ts
@@ -17,4 +17,31 @@ const hasPermission = (value: string | string[]): boolean => {
     throw new Error(t('permission.hasPermission'))
   }
   if (!isArray(value)) {
-    return permissions?.includes(value 
+    return permissions?.includes(value as string)
+  }
+  if (all_permission[0] === permissions[0]) {
+    return true
+  }
+  return (intersection(value, permissions) as string[]).length > 0
+}
+function hasPermi(el: Element, binding: DirectiveBinding) {
+  const value = binding.value
+
+  const flag = hasPermission(value)
+  if (!flag) {
+    el.parentNode?.removeChild(el)
+  }
+}
+const mounted = (el: Element, binding: DirectiveBinding<any>) => {
+  hasPermi(el, binding)
+}
+
+const permiDirective: Directive = {
+  mounted
+}
+
+export const setupPermissionDirective = (app: App<Element>) => {
+  app.directive('hasPermi', permiDirective)
+}
+
+export default permiDirective
