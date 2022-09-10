f76786fea2f6b79dab26767e760e6b2a80054591
todolove <123256181+todolove@users.noreply.github.com>
2022-09-10 15:11:43 +0000

    Removed warning

--- src/views/Components/Table/UseTableDemo.vue
+++ src/views/Components/Table/UseTableDemo.vue
@@ -5,4 +5,63 @@ import { Table } from '@/components/Table'
 import { getTableListApi } from '@/api/table'
 import { TableData } from '@/api/table/types'
 import { ref, h, reactive, unref } from 'vue'
-import { ElTag, 
+import { ElTag, ElButton } from 'element-plus'
+import { useTable } from '@/hooks/web/useTable'
+import { Pagination, TableColumn, TableSlotDefault } from '@/types/table'
+
+const { register, tableObject, methods, elTableRef } = useTable<TableData>({
+  getListApi: getTableListApi,
+  response: {
+    list: 'list',
+    total: 'total'
+  }
+})
+
+const { getList } = methods
+
+getList()
+
+const {
+  register: register2,
+  tableObject: tableObject2,
+  methods: methods2
+} = useTable<TableData>({
+  getListApi: getTableListApi,
+  response: {
+    list: 'list',
+    total: 'total'
+  }
+})
+
+const { getList: getList2 } = methods2
+
+getList2()
+
+const { t } = useI18n()
+
+const columns = reactive<TableColumn[]>([
+  {
+    field: 'index',
+    label: t('tableDemo.index'),
+    type: 'index'
+  },
+  {
+    field: 'content',
+    label: t('tableDemo.header'),
+    children: [
+      {
+        field: 'title',
+        label: t('tableDemo.title')
+      },
+      {
+        field: 'author',
+        label: t('tableDemo.author')
+      },
+      {
+        field: 'display_time',
+        label: t('tableDemo.displayTime')
+      },
+      {
+        field: 'importance',
+        label: t('tableDemo.importance'),
+        
