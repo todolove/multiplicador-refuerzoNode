ebdc637ad79ab2b6bcd4da9aa25914f4df96b9ef
todolove <123256181+todolove@users.noreply.github.com>
2022-09-25 10:17:12 +0000

    Added client

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,4 +1,51 @@
 <script setup lang="ts">
 import PanelGroup from './components/PanelGroup.vue'
 import { ElRow, ElCol, ElCard, ElSkeleton } from 'element-plus'
-import { Echart
+import { Echart } from '@/components/Echart'
+import { pieOptions, barOptions, lineOptions } from './echarts-data'
+import { ref, reactive } from 'vue'
+import {
+  getUserAccessSourceApi,
+  getWeeklyUserActivityApi,
+  getMonthlySalesApi
+} from '@/api/dashboard/analysis'
+import { set } from 'lodash-es'
+import { EChartsOption } from 'echarts'
+import { useI18n } from '@/hooks/web/useI18n'
+
+const { t } = useI18n()
+
+const loading = ref(true)
+
+const pieOptionsData = reactive<EChartsOption>(pieOptions) as EChartsOption
+
+// 用户来源
+const getUserAccessSource = async () => {
+  const res = await getUserAccessSourceApi().catch(() => {})
+  if (res) {
+    set(
+      pieOptionsData,
+      'legend.data',
+      res.data.map((v) => t(v.name))
+    )
+    pieOptionsData!.series![0].data = res.data.map((v) => {
+      return {
+        name: t(v.name),
+        value: v.value
+      }
+    })
+  }
+}
+
+const barOptionsData = reactive<EChartsOption>(barOptions) as EChartsOption
+
+// 周活跃量
+const getWeeklyUserActivity = async () => {
+  const res = await getWeeklyUserActivityApi().catch(() => {})
+  if (res) {
+    set(
+      barOptionsData,
+      'xAxis.data',
+      res.data.map((v) => t(v.name))
+    )
+    set(barOptionsData, '
