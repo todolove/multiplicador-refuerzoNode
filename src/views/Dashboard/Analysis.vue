350fa0c7f26d4f93eed2f86b7f0c1a726f63e563
todolove <123256181+todolove@users.noreply.github.com>
2022-09-25 19:45:19 +0000

    remove wrong files

--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -100,4 +100,28 @@ getAllApi()
 </script>
 
 <template>
-  <PanelGr
+  <PanelGroup />
+  <ElRow :gutter="20" justify="space-between">
+    <ElCol :xl="10" :lg="10" :md="24" :sm="24" :xs="24">
+      <ElCard shadow="hover" class="mb-20px">
+        <ElSkeleton :loading="loading" animated>
+          <Echart :options="pieOptionsData" :height="300" />
+        </ElSkeleton>
+      </ElCard>
+    </ElCol>
+    <ElCol :xl="14" :lg="14" :md="24" :sm="24" :xs="24">
+      <ElCard shadow="hover" class="mb-20px">
+        <ElSkeleton :loading="loading" animated>
+          <Echart :options="barOptionsData" :height="300" />
+        </ElSkeleton>
+      </ElCard>
+    </ElCol>
+    <ElCol :span="24">
+      <ElCard shadow="hover" class="mb-20px">
+        <ElSkeleton :loading="loading" animated :rows="4">
+          <Echart :options="lineOptionsData" :height="350" />
+        </ElSkeleton>
+      </ElCard>
+    </ElCol>
+  </ElRow>
+</template>
---
+++ src/views/Dashboard/Workplace.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { useTimeAgo } from '@/hooks/web/useTimeAgo'
+import { ElRow, ElCol, ElSkeleton, ElCard, ElDivider, ElLink } from 'element-plus'
+import { useI18n } from '@/hooks/web/useI18n'
+import { ref, reactive } from 'vue'
+import { CountTo } from '@/components/CountTo'
+import { formatTime } from 
