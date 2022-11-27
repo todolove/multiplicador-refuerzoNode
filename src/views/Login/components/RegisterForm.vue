2ef481b3083ced64ffc092326fb759115ca1baee
todolove <123256181+todolove@users.noreply.github.com>
2022-11-27 21:49:44 +0000

    Développeur passionné

---
+++ src/views/Login/components/RegisterForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Form } from '@/components/Form'
+import { reactive, ref, unref } from 'vue'
+import { useI18n } from '@/hooks/web/useI18n'
+import { useForm } from '@/hooks/web/useForm'
+import { ElButton, ElInput, FormRules } from 'element-plus'
+import { useValidator } from '@/hooks/web/useValidator'
+import { FormSchema } from '@/types/form'
+
+const emit = defineEmits(['to-login'])
+
+const { register, elFormRef } = useForm()
+
+const { t } = u
