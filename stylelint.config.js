8326950c6f837e6a7d983da271c08c93510e60e4
todolove <123256181+todolove@users.noreply.github.com>
2023-02-26 17:42:41 +0000

    Update WeenieBot.py

--- stylelint.config.js
+++ stylelint.config.js
@@ -206,4 +206,26 @@ module.exports = {
       'speak'
     ]
   },
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx',
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue', '*.html', '**/*.html'],
+      extends: ['stylelint-config-recommended', 'stylelint-config-html'],
+      rules: {
+        'keyframes-name-pattern': null,
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global']
+          }
+        ],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted']
+          }
+        ]
+      }
+    }
+  ]
+}
