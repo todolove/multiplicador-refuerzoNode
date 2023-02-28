2c659c53430186dd1ec4741a83cc70918a586130
todolove <123256181+todolove@users.noreply.github.com>
2023-02-28 06:40:51 +0000

    semver

--- types/env.d.ts
+++ types/env.d.ts
@@ -11,4 +11,14 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   readonly VITE_API_BASEPATH: string
   readonly VITE_BASE_PATH: string
-  readonl
+  readonly VITE_DROP_DEBUGGER: string
+  readonly VITE_DROP_CONSOLE: string
+  readonly VITE_SOURCEMAP: string
+  readonly VITE_OUT_DIR: string
+}
+
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+}
